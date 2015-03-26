2e523167197de5f297df371a795ae9d8ec6cb1fd
Arnaud Ligny <arnaud@ligny.org>
2015-03-26 01:17:06 +0100

    Update build.php

--- build.php
+++ build.php
@@ -1,19 +1,20 @@
 #!/usr/local/bin/php
 <?php
 require_once 'vendor/autoload.php';
-
 use PHPoole\PHPoole;
 
-$phpoole = new PHPoole('./', null, [
+$options = [
     'site' => [
         'title'       => "Demo blog",
         'baseline'    => 'This is a demo blog',
-        'baseurl'     => 'http://localhost:8000',
-        'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        'baseurl'     => 'http://localhost:8000/',
+        'description' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     ],
     'theme' => 'hyde'
-]);
-$phpoole->build();
+];
+
+PHPoole::create('./', null, $options)
+    ->build();
 
 echo "Start server http://localhost:8000\n";
 echo "Ctrl-C to stop it\n";
