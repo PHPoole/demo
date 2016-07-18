f7e0d962eb75b82b344ce4e1678c6bf1d9d130d3
Narno <arnaud@ligny.org>
2016-07-18 12:11:51 +0200

    Loads config from a Yaml file.

--- build.php
+++ build.php
@@ -1,45 +1,18 @@
 #!/usr/local/bin/php
 <?php
+if (php_sapi_name() !== 'cli') {
+    return;
+}
 date_default_timezone_set('Europe/Paris');
 require_once 'vendor/autoload.php';
+
 use PHPoole\PHPoole;
+use Symfony\Component\Yaml\Yaml;
 
-$options = [
-    'site' => [
-        'title'       => "PHPoole demo blog",
-        'baseline'    => 'This is a PHPoole demo blog',
-        'baseurl'     => 'http://localhost:8000/',
-        'description' => 'A demo blog based on Hyde, built with PHPoole-library.',
-        'menu' => [
-            'main' => [
-                'download' => [
-                    'id'     => 'download',
-                    'name'   => 'Download',
-                    'url'    => 'http://github.com/PHPoole/PHPoole-theme-hyde',
-                    'weight' => 50,
-                ],
-                'github' => [
-                    'id'     => 'github',
-                    'name'   => 'GitHub project',
-                    'url'    => 'http://github.com/PHPoole/PHPoole-theme-hyde',
-                    'weight' => 60,
-                ],
-            ],
-        ],
-    ],
-    'theme' => 'hyde',
-    'paginate' => [
-        'disabled' => true,
-        'homepage' => [
-            'section' => 'blog',
-        ],
-    ],
-];
+$options = Yaml::parse(file_get_contents('phpoole.yml'));
 
-PHPoole::create('./', null, $options)
-    ->build();
+PHPoole::create($options)->build();
 
 echo "Start server http://localhost:8000\n";
 echo "Ctrl-C to stop it\n";
-//exec('php -S localhost:8000 -t _site/ >/dev/null');
 exec('php -S localhost:8000 -t _site');
