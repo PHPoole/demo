26e2469391505d1dd39d184b6652e7fc82b283a9
Arnaud Ligny <arnaud@ligny.org>
2016-08-18 00:28:51 +0200

    Auto-deploy GH page. (#5)
    * Auto-deploy GH page.
    
    * Fixing build script.
    
    * Fixing baseurl.
    
    * Updates readme.

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 
 This [PHPoole](https://github.com/Narno/PHPoole) demo is a simple blog, based on the [Hyde](https://github.com/PHPoole/PHPoole-theme-hyde) theme.
 
+## Installation
+
 ```
 $ composer create-project narno/phpoole-demo
 ```
-Then with [PHPoole CLI](https://github.com/Narno/PHPoole):
+
+## Usage
+
+With [PHPoole CLI](https://github.com/Narno/PHPoole):
 ```
-$ phpoole build phpoole-demo
+$ cd phpoole-demo
+$ phpoole build -s
 ```
-Or with the included PHP script:
+
+**Or** with the included PHP script:
 ```
-$ composer require narno/phpoole-library
 $ cd phpoole-demo
 $ php build.php
 ```
--- build.php
+++ build.php
@@ -5,14 +5,26 @@ if (php_sapi_name() !== 'cli') {
 }
 date_default_timezone_set('Europe/Paris');
 require_once 'vendor/autoload.php';
-
 use PHPoole\PHPoole;
 use Symfony\Component\Yaml\Yaml;
 
+$getopt = getopt('e::');
+
 $options = Yaml::parse(file_get_contents('phpoole.yml'));
+$options_dev = [
+    'site' => [
+        'baseurl' => 'http://localhost:8000',
+    ],
+];
 
-PHPoole::create($options)->build();
+$prod = (isset($getopt['e']) && $getopt['e'] == 'prod') ? true : false;
+$options = (!$prod) ? array_replace_recursive($options, $options_dev) : $options;
 
-echo "Start server http://localhost:8000\n";
-echo "Ctrl-C to stop it\n";
-exec('php -S localhost:8000 -t _site');
+$phpoole = new PHPoole($options);
+$phpoole->build();
+
+if (!$prod) {
+    echo "Start server http://localhost:8000\n";
+    echo "Ctrl-C to stop it\n";
+    exec('php -S localhost:8000 -t _site');
+}
